a9a5deeeba1b96d934338b5491e0788a418f8fe8
FattiMei <teomei68@gmail.com>
2024-01-16 19:35:57 +0100

    Initial commit, again

---
+++ lua/FattiMei/init.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+
+require("FattiMei.set")
+require("FattiMei.remap")
+require("FattiMei.lazy")
---
+++ lua/FattiMei/lazy.lua
@@ -0,0 +1,32 @@
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system {
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
+		lazypath,
+	}
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+require("lazy").setup("FattiMei.plugins")
+
+
+--require('lazy').setup({
+--	{
+--		'folke/which-key.nvim',
+--		event = "VeryLazy",
+--		init = function()
+--			vim.o.timeout = true
+--			vim.o.timeoutlen = 500
+--		end,
+--
+--		opts = {}
+--	},
+--
+--
+--
+--})
